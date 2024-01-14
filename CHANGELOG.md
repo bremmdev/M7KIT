6727ea0a098dffe54c6d042750074b2e1ca351ed
Matt Bremmers <bremmdev@gmail.com>
2024-01-14 14:56:34 +0100

    feat: add autodocs for storybook

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
-## 0.1.0 - 2024-01-13
+0.1.0 - 2024-01-13
 
 ### Added
 - `FolderStructure` component
 - Add TailwindCSS for styling
+
+0.2.0 - 2024-01-14
+
+### Added
+
+- Autodocs for Storybook
--- src/FolderStructure/FolderStructure.stories.tsx
+++ src/FolderStructure/FolderStructure.stories.tsx
@@ -1,52 +1,87 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
-import { FolderStructure } from './FolderStructure';
+import { FolderStructure } from "./FolderStructure";
 
 const meta: Meta<typeof FolderStructure> = {
   component: FolderStructure,
+  title: "Components/FolderStructure",
+  tags: ["autodocs"],
+  parameters: {
+    docs: {
+      description: {
+        component: 'Component for displaying hierarchical data structures. It uses Folder and File subcomponents to represent nodes in a file-tree hierarchy. It use an Indentation subcomponent to visually represents the depth of each node.',
+      },
+    },
+  },
 };
 export default meta;
 
 type Story = StoryObj<typeof FolderStructure>;
 
+/*
+**  Basic: Flat structure with no nesting
+*/
+
 export const Basic: Story = {
   args: {
     data: {
-      name: 'src',
+      name: "Indentation",
       children: [
         {
-          name: 'FolderStructure',
-          children: [
-            {
-              name: 'FolderStructure.tsx',
-            },
-            {
-              name: 'FolderStructure.types.ts',
-            },
-            {
-              name: 'FolderStructure.stories.tsx',
-            },
-          ],
+          name: "Indentation.tsx",
         },
         {
-          name: 'Indentation',
-          children: [
-            {
-              name: 'Indentation.tsx',
-            },
-            {
-              name: 'Indentation.types.ts',
-            },
-            {
-              name: 'Indentation.stories.tsx',
-            },
-          ],
+          name: "Indentation.types.ts",
         },
         {
-          name: 'index.tsx',
+          name: "Indentation.stories.tsx",
         },
       ],
     },
-    }
+  },
 };
 
+/*
+**  Nested: Nested structure with multiple levels
+*/
+
+export const Nested: Story = {
+  args: {
+    data: {
+      name: "src",
+      children: [
+        {
+          name: "FolderStructure",
+          children: [
+            {
+              name: "Indentation",
+              children: [
+                {
+                  name: "Indentation.tsx",
+                },
+                {
+                  name: "Indentation.types.ts",
+                },
+                {
+                  name: "Indentation.stories.tsx",
+                },
+              ],
+            },
+            {
+              name: "FolderStructure.tsx",
+            },
+            {
+              name: "FolderStructure.types.ts",
+            },
+            {
+              name: "FolderStructure.stories.tsx",
+            },
+          ],
+        },
+        {
+          name: "index.tsx",
+        },
+      ],
+    },
+  },
+};
